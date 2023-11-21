55aa3953e1adaa40d2898ddd425717ea629dfb65
Arshad882 <125073434+Arshad882@users.noreply.github.com>
2023-11-20 20:46:46 -0500

    Add files via upload

---
+++ SQLQuery.sql
@@ -0,0 +1,129 @@
+
+insert into certifications values('AOCN','Advanced Oncology Certified Nurse','oncc.org');
+insert into certifications values('AOCNP','Advanced Oncology Certified Nurse Practitioner','oncc.org');
+insert into certifications values('CCCN','Certified Continence Care Nurse','wocncb.org');
+insert into certifications values('COCN','Certified Ostomy Care Nurse','wocncb.org');
+insert into certifications values('NCSN','National Certified School Nurse','ncbsn.org');
+insert into certifications values('OCN','Oncology Certified Nurse','oncc.org');
+insert into certifications values('PCCN','Progressive Care Nursing','aacn.org');
+insert into certifications values('PHMS','Pediatric Primary Care Mental Health Specialist','pncb.org');
+insert into certifications values('TCRN','Trauma Certified Registered Nurse','nccwebsite.org');
+insert into certifications values('SCRN','Stroke Certified Registered Nurse','nccwebsite.org'); 
+
+insert into Insurance_Company values('321','United Health','895273613','Tampa','Florida','33654');
+insert into Insurance_Company values('322','Kaiser Foundation','453376193','Tampa','Florida','33654');
+insert into Insurance_Company values('323','CVS Health','895273456','Tampa','Florida','33654');
+insert into Insurance_Company values('324','Molina Healthcare','678908193','Tampa','Florida','33654');
+insert into Insurance_Company values('325','Caresource','895124593','Tampa','Florida','33654');
+insert into Insurance_Company values('326','Carefirst Inc','89526789','Tampa','Florida','33654');
+insert into Insurance_Company values('327','Metropolitan','765433193','Tampa','Florida','33654');
+insert into Insurance_Company values('328','Point32Health','89527680','Tampa','Florida','33654');
+insert into Insurance_Company values('329','Highmark Group','987668909','Tampa','Florida','33654');
+insert into Insurance_Company values('320','Local Initiative Health ','895776786','Tampa','Florida','33654');
+
+insert into Patient values('200','Port','Leis','Smith','45','Park club drive','Albany','California');
+insert into Patient values('201','ram','sita','roy','32','Park club drive','Tampa','Florida');
+insert into Patient values('202','laxa','lere','mith','35','Park drive','Tampa','Florida');
+insert into Patient values('203','Pert','teis','tmith','43','club drive','Albany','California');
+insert into Patient values('204','Roth','heiv','kmitr','65','Park club ','Albany','California');
+insert into Patient values('205','tony','oeis','york','33','Park Area ','Albany','California');
+insert into Patient values('206','urfi','Leis','guith','35','univety club ','Texas','TX');
+insert into Patient values('207','they','himesr','thesis','40','Park  drive','Albany','California');
+insert into Patient values('208','jocb','jogr','Smith','55','Arsa club drive','Tampa','Florida');
+insert into Patient values('209','hive','levs','Somth','25','Areas club','Tampa','Folrida');
+
+
+insert into Claim values('455677','321','200','TB','650');
+insert into Claim values('455677','322','201','Fever','750');
+insert into Claim values('455677','323','202','Aids','680');
+insert into Claim values('455677','324','203','COVID','580');
+insert into Claim values('455677','325','204','COVID','5850');
+insert into Claim values('455677','326','205','COLD','5785');
+insert into Claim values('455677','327','206','PAin','6454');
+insert into Claim values('455677','328','207','TB','3656');
+insert into Claim values('455677','329','208','COVID','2550');
+insert into Claim values('455677','330','209','TB','65450');
+
+insert into Department values('6786823','1234533');
+insert into Department values('6786545','1444533');
+insert into Department values('6742525','4524533');
+insert into Department values('6736353','4526533');
+insert into Department values('6735683','7854533');
+insert into Department values('6836823','8474533');
+insert into Department values('6386823','7985533');
+insert into Department values('7989823','4794533');
+insert into Department values('8956823','9368533');
+insert into Department values('5955823','4966533');
+
+
+insert into Doctor values('Josh','Tribiani','56789','8496372132','11','100');
+insert into Doctor values('Andew','rquiani','33389','8434541555','12','101');
+insert into Doctor values('Arshd','urey','45589','8415445515','13','102');
+insert into Doctor values('Joserh','jgyuty','75679','2451213452','14','103');
+insert into Doctor values('handt','asraa','55779','8453445132','15','104');
+insert into Doctor values('hatr','asas','56779','8145451341','16','105');
+insert into Doctor values('joiue','jyothi','57657','3145134132','17','106');
+insert into Doctor values('louy','joit','67389','1345145132','18','107');
+insert into Doctor values('goue','koit','63769','3451532132','19','108');
+insert into Doctor values('loki','guny','78359','8345134451','20','109');
+
+
+insert into Degree values('89734','masters','MBBS','University of Cincinati','2015');
+insert into Degree values('89745','masters','MBBS','University of Florida','2018');
+insert into Degree values('89565','masters','MBBS','University of Cincinati','2017');
+insert into Degree values('89767','masters','MBBS','University of ohio','2019');
+insert into Degree values('89735','masters','MBBS','University of Cincinati','2017');
+insert into Degree values('89357','masters','MBBS','University of Florida','2005');
+insert into Degree values('89764','masters','MBBS','University of Florida','2015');
+insert into Degree values('89367','masters','MBBS','University of ohio','2013');
+insert into Degree values('89784','masters','MBBS','University of ohio','2009');
+insert into Degree values('89989','masters','MBBS','University of Florida','2007');
+
+insert into Nurse values('800','Chandler','Bing','12345','1980','800','200');
+insert into Nurse values('801','ulfddler','gipg','12334','1989','801','201');
+insert into Nurse values('802','tong','hipng','12334','1958','802','202');
+insert into Nurse values('803','thisy','hiojj','12342','1909','803','203');
+insert into Nurse values('804','uhrndler','king','12324','1967','804','204');
+insert into Nurse values('805','jhon','gipoj','12654','1996','805','205');
+insert into Nurse values('806','gunfler','Bing','12878','1990','806','206');
+insert into Nurse values('807','joingdler','joih','12869','1909','807','207');
+insert into Nurse values('808','jorth','koito','12897','1987','808','208');
+insert into Nurse values('809','jordan','koyto','12768','1988','809','209');
+
+insert into Operation_Type values('surgery','2:00','8860');
+insert into Operation_Type values('surgery','7:00','7560'); 
+insert into Operation_Type values('surgery','7:40','2560'); 
+insert into Operation_Type values('surgery','3:00','3450'); 
+insert into Operation_Type values('surgery','7:20','5340'); 
+insert into Operation_Type values('surgery','2:54','3240'); 
+insert into Operation_Type values('surgery','2:23','2530'); 
+insert into Operation_Type values('surgery','3:30','4550'); 
+insert into Operation_Type values('surgery','2:40','5400'); 
+insert into Operation_Type values('surgery','7:50','4300'); 
+
+
+
+insert into Operation values('200','11','surgery','2022-02-10','10:00','18:30','9'); 
+insert into Operation values('201','12','surgery','2022-02-18','13:00','17:30','5'); 
+insert into Operation values('202','13','surgery','2022-02-17','10:00','16:30','6'); 
+insert into Operation values('203','14','surgery','2022-02-16','15:00','15:30','65'); 
+insert into Operation values('204','15','surgery','2022-02-17','16:00','12:30','65'); 
+insert into Operation values('205','16','surgery','2022-02-23','18:00','08:30','54'); 
+insert into Operation values('206','17','surgery','2022-02-21','19:00','17:30','54'); 
+insert into Operation values('207','18','surgery','2022-02-24','16:00','13:30','75'); 
+insert into Operation values('208','19','surgery','2022-02-25','21:00','15:30','56'); 
+insert into Operation values('209','20','surgery','2022-02-27','13:00','16:30','99'); 
+
+
+
+select * from certifications; 
+select * from Insurance_Company;
+select * from Patient;
+select * from Claim;  
+select * from Department;
+select * from Doctor;
+select * from Degree; 
+select * from Nurse; 
+select * from Operation_Type;
+select * from Operation;
+ 
